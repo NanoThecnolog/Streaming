5d06d1715f81b7165aa3b5ab7ff00219973a1229
NanoThecnolog <contato@ericssongomes.com>
2024-09-26 18:32:47 -0300

    ajustando retorno de listagem de usuarios

--- backend/src/Services/User/ListUserService.ts
+++ backend/src/Services/User/ListUserService.ts
@@ -2,7 +2,18 @@ import prismaClient from "../../prisma";
 
 class ListUserService {
     async execute() {
-        const users = await prismaClient.user.findMany();
+        const users = await prismaClient.user.findMany({
+            select: {
+                id: true,
+                name: true,
+                email: true,
+                birthday: true,
+                watchLater: true,
+                donator: true,
+                avatar: true,
+                verified: true
+            }
+        });
 
         return users
     }
