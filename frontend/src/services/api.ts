0adead6b90dc1a5904bb6db795d015d6133fc6d8
NanoThecnolog <contato@ericssongomes.com>
2024-10-05 12:02:28 -0300

    corrigindo baseURL pra utilizar backend online

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -5,5 +5,5 @@ const backendDev = "http://localhost:3333"
 //criar uma variável de ambiente para que o controle da url baseado no ambiente seja feito de forma automática
 
 export const api = axios.create({
-    baseURL: backendDev
+    baseURL: backendRender
 })
--- frontend/src/services/donate.ts
+++ frontend/src/services/donate.ts
@@ -5,7 +5,7 @@ const backendDev = "http://localhost:3333"
 //criar uma variável de ambiente para que o controle da url baseado no ambiente seja feito de forma automática
 
 export const donate = axios.create({
-    baseURL: `${backendDev}`
+    baseURL: `${backendRender}`
 })
 
 
