512a9590061b95236aa7b467efbed86a5f6f684b
NanoThecnolog <contato@ericssongomes.com>
2024-10-05 12:22:01 -0300

    Corrigindo nome de variável em uso

--- frontend/src/services/donate.ts
+++ frontend/src/services/donate.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const backendRender = "https://streaming-lj2o.onrender.com"
 const backendDev = "http://localhost:3333"
 //criar uma variável de ambiente para que o controle da url baseado no ambiente seja feito de forma automática
-const url = process.env.RENDER;
+const url = process.env.NEXT_PUBLIC_RENDER;
 if (!url) console.log("variável de ambiente não configurada.")
 
 export const donate = axios.create({
