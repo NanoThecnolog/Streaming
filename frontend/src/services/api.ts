0aaeaa350e90277f2d6194225faf40ab680e296f
NanoThecnolog <contato@ericssongomes.com>
2024-10-05 12:11:02 -0300

    Criando variavel de ambiente com url base do back

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -3,7 +3,9 @@ import axios from "axios";
 const backendRender = "https://streaming-lj2o.onrender.com"
 const backendDev = "http://localhost:3333"
 //criar uma variável de ambiente para que o controle da url baseado no ambiente seja feito de forma automática
+const url = process.env.RENDER;
+if (!url) console.log("variável de ambiente não configurada.")
 
 export const api = axios.create({
-    baseURL: backendRender
+    baseURL: url
 })
