dab0a8250682ad812fd893ee90cafbe51ae8a6e6
NanoThecnolog <contato@ericssongomes.com>
2024-10-29 13:11:55 -0300

    test(me): buscando local do erro

--- frontend/src/pages/me/index.tsx
+++ frontend/src/pages/me/index.tsx
@@ -106,7 +106,9 @@ export default function Me(status: { status: string }) {
                             <aside className={styles.asideContainer}>
                                 <div className={styles.avatar}>
                                     <div className={styles.imgContainer}>
-                                        <Image src={user?.avatar} alt="Avatar" width={150} height={150} />
+                                        {/*
+                                            <Image src={user?.avatar} alt="Avatar" width={150} height={150} />
+                                        */}
                                         <div className={styles.editAvatar}>
                                             <button type="button" title="Mudar Avatar" onClick={handleOpenModal}><BiSolidEditAlt size={20} /></button>
                                         </div>
@@ -127,7 +129,10 @@ export default function Me(status: { status: string }) {
                                 </div>
                                 <div className={styles.donate}>
                                     <h3>Torne-se um doador!!</h3>
-                                    <Qrcode />
+                                    {/*
+                                        <Qrcode />
+                                    */}
+
                                 </div>
                             </aside>
 
@@ -154,3 +159,49 @@ export const getServerSideProps: GetServerSideProps = async () => {
         }
     }
 }
+
+/*
+                            <section className={styles.sectionContainer}>
+                                <div className={styles.infoContainer}>
+                                    <h2 style={{ textAlign: 'center' }}>Sua Lista para assistir mais tarde</h2>
+                                    <div className={styles.watchLater}>
+                                        <div className={styles.filmes}>
+                                            <h4>Filmes</h4>
+                                            <div className={styles.watchContainer}>
+                                                {
+                                                    cards.filter(filme => user &&
+                                                        user.myList.length > 0 && user.myList.some(titulo => titulo.title === filme.title &&
+                                                            (titulo.subtitle === filme.subtitle || titulo.subtitle === '' || filme.subtitle === '')
+                                                        )
+                                                    ).map((filme, index) => (
+
+                                                        <div title={`Assistir ${filme.title}`} key={index} className={styles.watch}>
+                                                            <span onClick={() => handleWatch(filme)}>
+                                                                {filme.title}{filme.subtitle && <span> - {filme.subtitle}</span>}
+                                                            </span>
+                                                            <X onClick={() => handleRemove(filme.title, filme.subtitle)} /></div>
+                                                    ))
+                                                }
+                                            </div>
+                                        </div>
+                                        <div className={styles.series}>
+                                            <h4>Series</h4>
+                                            <div className={styles.watchContainer}>
+                                                {
+                                                    series.filter(serie => user &&
+                                                        user.myList.length && user.myList.some(titulo => titulo.title === serie.title &&
+                                                            (titulo.subtitle === serie.subtitle || titulo.subtitle === '' || serie.subtitle === '')
+                                                        )
+                                                    ).map((serie, index) => (
+                                                        <div title={`Assistir ${serie.title}`} key={index} className={styles.watch}>
+                                                            <span onClick={() => handleWatch(serie)}>
+                                                                {serie.title} {serie.subtitle && <span>- {serie.subtitle}</span>}
+                                                            </span>
+                                                            <X onClick={() => handleRemove(serie.title, serie.subtitle)} /></div>
+                                                    ))
+                                                }
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </section>*/
