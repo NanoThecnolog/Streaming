f9f202c6a9c1348d24faef55d5a5c1adb3f961ae
NanoThecnolog <contato@ericssongomes.com>
2024-11-02 20:24:57 -0300

    feat(vercel speed insights): Adiciona speed insights ao projeto para monitoramento de performance na Vercel

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -2,11 +2,13 @@ import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from "react-toastify";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
+      <SpeedInsights />
       <ToastContainer autoClose={3000} />
     </>
   )
