0c5b657323f60b974694c9798808bb504717ddfb
NanoThecnolog <contato@ericssongomes.com>
2024-11-20 17:40:51 -0300

    Feat(vercel analytics): Adiciona lib da Vercel para Analytics

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -3,12 +3,14 @@ import type { AppProps } from "next/app";
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from "react-toastify";
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from '@vercel/analytics/next';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
       <SpeedInsights />
+      <Analytics />
       <ToastContainer autoClose={3000} />
     </>
   )
