fe37ff6db2bfcf60e72fb2fb3f3d73df91909971
NanoThecnolog <contato@ericssongomes.com>
2025-03-07 22:25:58 -0300

    Feat(loading Texts): Adiciona multiplos textos a tela de carregamento

--- frontend/src/components/ui/Loading/index.tsx
+++ frontend/src/components/ui/Loading/index.tsx
@@ -1,6 +1,43 @@
+import { useEffect, useState } from 'react'
 import styles from './styles.module.scss'
+import { text } from '@/utils/Variaveis'
 
 export default function Loading() {
+    const [randomText, setRandomText] = useState<string>("")
+    const [shuffledTexts, setShuffledTexts] = useState<string[]>([])
+    const [index, setIndex] = useState<number>(0)
+
+    const shuffleArray = (arr: string[]) => {
+        return arr
+            .map((value) => ({ value, sort: Math.random() }))
+            .sort((a, b) => a.sort - b.sort)
+            .map(({ value }) => value)
+    }
+
+    useEffect(() => {
+        let current = shuffleArray(text)
+        setShuffledTexts(current)
+        setRandomText(current[0])
+
+        const timer = setInterval(() => {
+            setIndex((prevIndex) => {
+                let newIndex = prevIndex + 1;
+
+                if (newIndex >= text.length) {
+                    current = shuffleArray(text)
+                    setShuffledTexts(current)
+                    newIndex = 0
+                }
+                setRandomText(current[newIndex])
+                return newIndex
+            })
+            //const shuffling = text[Math.floor(Math.random() * text.length)]
+            //setRandomText(shuffling)
+
+        }, 4000)
+
+        return () => clearInterval(timer)
+    }, [])
     return (
         <div className={styles.loadingContainer}>
             <div>
@@ -8,7 +45,7 @@ export default function Loading() {
             </div>
             <div className={styles.spinner}></div>
             <div>
-                <h2>Carregando...</h2>
+                <h2>{randomText}</h2>
             </div>
         </div>
     )
