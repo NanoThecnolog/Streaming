f6ef494019999eb8e0c4b31d7d02a262f4804156
NanoThecnolog <contato@ericssongomes.com>
2025-03-26 18:39:23 -0300

    Fix(dotenv)

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,9 +2,13 @@ import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors'
 import { router } from './router';
 import axios from 'axios';
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express();
 const port = process.env.PORT || 3333;
+
 
 app.use(express.json({ limit: '40mb' }));
 app.use(express.urlencoded({ limit: '40mb', extended: true }));
