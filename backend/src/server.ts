3e0ca9ed26d9dd51e7ce5911a52558034b06d68b
NanoThecnolog <contato@ericssongomes.com>
2025-05-22 02:51:56 -0300

    Fix express-rate-limit

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -12,7 +12,7 @@ const port = process.env.PORT || 3333;
 
 app.use(express.json({ limit: '40mb' }));
 app.use(express.urlencoded({ limit: '40mb', extended: true }));
-//app.use(express.json());
+app.set('trust proxy', true)
 app.use(cors());
 app.use(router);
 
