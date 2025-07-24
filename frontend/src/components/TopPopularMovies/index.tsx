3f3c80aef3a3e392cbec92e85d0fa24e000797c2
NanoThecnolog <contato@ericssongomes.com>
2025-07-24 12:37:48 -0300

    Fix: Ajuste de renderização de baseCarousel.

--- frontend/src/components/TopPopularMovies/index.tsx
+++ frontend/src/components/TopPopularMovies/index.tsx
@@ -1,6 +1,5 @@
 import { CardsProps, MovieTMDB } from '@/@types/Cards'
 import BaseCarousel from '../ui/BaseCarousel'
-import { debug } from '@/classes/DebugLogger'
 import { useEffect, useState } from 'react'
 
 interface TopPopularProps {
@@ -14,14 +13,16 @@ export default function TopPopularMovies({ cardPerContainer, cards, moviesDB }: 
 
 
     useEffect(() => {
-        const mapMoviesDB = new Map(moviesDB.map(item => [item.tmdbId, item]))
-        const cardsPerPopularity = cards.sort((a, b) => b.popularity - a.popularity)
-        const carouselMovies: CardsProps[] = cardsPerPopularity
-            .map(card => mapMoviesDB.get(card.id))
-            .filter((item): item is CardsProps => item !== undefined)
-            .slice(0, 10)
+        if (movies.length === 0) {
+            const mapMoviesDB = new Map(moviesDB.map(item => [item.tmdbId, item]))
+            const cardsPerPopularity = cards.sort((a, b) => b.popularity - a.popularity)
+            const carouselMovies: CardsProps[] = cardsPerPopularity
+                .map(card => mapMoviesDB.get(card.id))
+                .filter((item): item is CardsProps => item !== undefined)
+                .slice(0, 10)
+            setMovies(carouselMovies)
+        }
 
-        setMovies(carouselMovies)
     }, [cards, moviesDB])
 
     if (movies.length === 0) return
--- frontend/src/components/ui/BaseCarousel/index.tsx
+++ frontend/src/components/ui/BaseCarousel/index.tsx
@@ -21,35 +21,32 @@ export default function BaseCarousel({ title, cardPerContainer, cards }: BasePro
     return (
         <div className={styles.carouselContainer}>
             <h2 className={styles.contentTitle}>{title.toUpperCase()}</h2>
+
             {
-                cardPerContainer && (
-                    <>
-                        {debug.log('cards', cards.map(c => 'season' in c ? c.tmdbID : c.tmdbId))}
-                        <Swiper
-                            //ref={swiperRef}
-                            modules={[Navigation]}
-                            spaceBetween={10}
-                            slidesPerView={cardPerContainer}
-                            loop={false}
-                            className={styles.carousel}
-                        >
-                            {cards.map((card, index) => {
-                                if ('season' in card) {
-                                    return <SwiperSlide key={uniqueKey(card)} className={styles.slide}>
-                                        <Link href={`/series/serie/${card.tmdbID}`}><p>{index + 1}</p></Link>
-                                        <Card card={card} />
-                                    </SwiperSlide>
-                                } else {
-                                    return <SwiperSlide key={uniqueKey(card)} className={styles.slide}>
-                                        <Link href={`/movie/${card.tmdbId}`}><p>{index + 1}</p></Link>
-                                        <Card card={card} />
-                                    </SwiperSlide>
-                                }
-                            })}
-                        </Swiper>
-                    </>
-                )
+                //debug.log('cards', cards.map(c => 'season' in c ? c.tmdbID : c.tmdbId))
             }
+            <Swiper
+                //ref={swiperRef}
+                modules={[Navigation]}
+                spaceBetween={10}
+                slidesPerView={cardPerContainer}
+                loop={false}
+                className={styles.carousel}
+            >
+                {cards.map((card, index) => {
+                    if ('season' in card) {
+                        return <SwiperSlide key={uniqueKey(card)} className={styles.slide}>
+                            <Link href={`/series/serie/${card.tmdbID}`}><p>{index + 1}</p></Link>
+                            <Card card={card} />
+                        </SwiperSlide>
+                    } else {
+                        return <SwiperSlide key={uniqueKey(card)} className={styles.slide}>
+                            <Link href={`/movie/${card.tmdbId}`}><p>{index + 1}</p></Link>
+                            <Card card={card} />
+                        </SwiperSlide>
+                    }
+                })}
+            </Swiper>
         </div>
     )
 }
