7c7a797b234048af4cc5f23a8f416a07a60e5754
NanoThecnolog <contato@ericssongomes.com>
2025-09-21 15:39:22 -0300

    Ajusta scripts de build e postbuild. Ajusta config do sitemap

--- frontend/next-sitemap.config.js
+++ frontend/next-sitemap.config.js
@@ -1,9 +1,11 @@
+const { config } = require('googleapis/build/src/apis/config');
+
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://flixnext.com.br', // URL base
-    generateRobotsTxt: true, // Geração automática do robots.txt
-    changefreq: 'daily', // Frequência de rastreamento
-    priority: 0.7, // Prioridade padrão para as páginas
+    siteUrl: 'https://flixnext.com.br',
+    generateRobotsTxt: true,
+    changefreq: 'daily',
+    priority: 0.7,
     robotsTxtOptions: {
         policies: [
             { userAgent: '*', allow: '/' },
@@ -11,8 +13,30 @@ module.exports = {
             { userAgent: '*', allow: '/planos' },
             { userAgent: '*', allow: '/movie/*' },
             { userAgent: '*', allow: '/series/serie/*' },
+            { userAgent: '*', allow: '/signup' },
+            { userAgent: '*', allow: '/login' },
             { userAgent: '*', disallow: '/watch', },
             { userAgent: '*', disallow: '/recover', },
         ],
     },
+    transform: async (config, path) => {
+        let priority = config.priority
+
+        if (path === '/') {
+            priority = 1.0
+        }
+        if (path === '/signup') {
+            priority = 0.9
+        }
+        if (path === '/login') {
+            priority = 0.8
+        }
+
+        return {
+            loc: path,
+            changefreq: config.changefreq,
+            priority,
+            lastmod: new Date().toISOString()
+        }
+    }
 };
