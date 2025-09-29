470bc53876d43bacb5be261fd57754324b799a9c
NanoThecnolog <contato@ericssongomes.com>
2025-09-29 14:47:34 -0300

    Altera serie de destaque

--- frontend/src/pages/series/index.tsx
+++ frontend/src/pages/series/index.tsx
@@ -32,7 +32,7 @@ export default function Series() {
     const { serieData, setSerieData, setAllData, allData } = useTMDB()
     const [visible, setvisible] = useState(false)
     const { series, setSeries } = useFlix()
-    const tmdbid = 119051;
+    const tmdbid = 157239;
     const [topCard, setTopCard] = useState<SeriesProps | null>(null)
 
     useEffect(() => {
