413288c6d115e6a89111c863cea7e0c69a2eaeb9
NanoThecnolog <contato@ericssongomes.com>
2025-12-14 21:37:32 -0300

    Ajusta trust proxy

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -12,7 +12,7 @@ const port = process.env.PORT || 3333;
 
 app.use(express.json({ limit: '40mb' }));
 app.use(express.urlencoded({ limit: '40mb', extended: true }));
-app.set('trust proxy', true)
+app.set('trust proxy', 1)
 app.use(cors());
 app.use(router);
 
