9ccfd5bd6cc95021ffa30ded2e2019e12b50247e
NanoThecnolog <contato@ericssongomes.com>
2025-12-14 23:41:20 -0300

    Ajusta responsividade da pagina de pegamentos

--- frontend/src/pages/api/user/detail.ts
+++ frontend/src/pages/api/user/detail.ts
@@ -1,6 +1,7 @@
 // pages/api/user-data.ts
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { SetupAPIClient } from '@/services/api'
+import { debug } from '@/classes/DebugLogger'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     const client = new SetupAPIClient({ req })
@@ -9,6 +10,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         const response = await client.api.get('/user/access')
         return res.status(200).json(response.data)
     } catch (err) {
+        debug.log("Erro ao buscar dados no backend", err)
         return res.status(500).json({ error: 'Erro ao buscar dados.' })
     }
 }
