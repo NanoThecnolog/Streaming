4d6c810cb746aecdad22ad283d283db52e33561a
NanoThecnolog <contato@ericssongomes.com>
2025-12-19 14:03:28 -0300

    Ajusta hook useWarningAcessModal

--- frontend/src/hooks/useWarningAccessModal.ts
+++ frontend/src/hooks/useWarningAccessModal.ts
@@ -1,11 +1,15 @@
 // src/hooks/useDailyModal.ts
+import { useFlix } from '@/contexts/FlixContext';
 import { useEffect, useState } from 'react';
 
 export function useWarningAccessModal(key: boolean) {
     const [isOpen, setIsOpen] = useState(false);
+    const { user } = useFlix()
 
     useEffect(() => {
         if (typeof window === 'undefined') return;
+        if (!user) return
+        if (user.donator) return
 
         if (!key) {
             setIsOpen(true)
